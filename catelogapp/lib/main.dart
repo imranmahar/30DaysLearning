6e985b70e88a438ac9bc6eec0728309d9dd17af3
exconsol <kamran.mahar@gmail.com>
2022-09-27 13:26:07 +0500

    Day 4 Complete

--- catelogapp/lib/main.dart
+++ catelogapp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:catelogapp/Pages/Home_Page.dart';
 import 'package:catelogapp/Pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(myApp());
@@ -13,7 +14,10 @@ class myApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       //home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
